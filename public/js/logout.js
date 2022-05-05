34ec91aed666492b50b800e8aa4fcaa982bf17f4
Maria Brown <mariabrown0908@gmail.com>
2022-05-05 10:36:06 -0400

    updated offers function

--- public/js/logout.js
+++ public/js/logout.js
@@ -11,5 +11,29 @@ const logout = async () => {
     }
   };
   
-  document.querySelector('#logout').addEventListener('click', logout);
+  // offers
+  
+  const offersFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const date_available = document.querySelector('#date_available').value;
+    const location = document.querySelector('#location').value;
+    const crop = document.querySelector('#crop').value;
+  const quantity = document.querySelector('#quantity').value;
+  
+    if (date_available && crop && location) {
+      const response = await fetch(/api/users, {
+        method: 'POST',
+        body: JSON.stringify({ date_available,crop,quantity}),
+        headers: { 'Content-Type': application/json}
+      
+      });
+  
+      if (response.ok) {
+        document.location.replace('/offers');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
   
